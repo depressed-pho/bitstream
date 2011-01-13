c26aed1fa9720443519130bf6d6f4c1fddfb9113
PHO <pho@cielonegro.org>
2011-01-14 01:57:45 +0900

    Data.Bitstream

--- Data/Bitstream.hs
+++ Data/Bitstream.hs
@@ -1,8 +1,23 @@
 {-# LANGUAGE
     UnicodeSyntax
   #-}
--- | FIXME
+-- | Fast, packed, strict bit vectors using stream fusion.
+--
+-- This module is intended to be imported @qualified@, to avoid name
+-- clashes with "Prelude" functions. e.g.
+--
+-- > import qualified Data.BitStream as S
+--
+-- FIXME: explain about directions
 module Data.Bitstream
-    (
+    ( Bitstream
+    , Left
+    , Right
     )
     where
+import Data.Bitstream.BitChunk (Left, Right, Chunk)
+--import qualified Data.Bitstream.BitChunk as C
+import qualified Data.StorableVector as S
+
+newtype Bitstream d
+    = BitStream (S.Vector (Chunk d))
