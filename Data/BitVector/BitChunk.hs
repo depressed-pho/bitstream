0b346592f02961b4bca648772648cd9f4230dc76
PHO <pho@cielonegro.org>
2011-01-13 18:11:09 +0900

    BitChunk: foldl

--- Data/BitVector/BitChunk.hs
+++ Data/BitVector/BitChunk.hs
@@ -27,11 +27,14 @@ module Data.BitVector.BitChunk
 
     , map
     , reverse
+
+    , foldl
     )
     where
 import Data.Bits
 import Data.Word
-import Prelude hiding (length, map, null, reverse)
+import Prelude hiding (foldl, length, map, null, reverse)
+import Prelude.Unicode
 
 data Chunk d = Chunk {-# UNPACK #-} !Int 
                      {-# UNPACK #-} !Word8
@@ -52,6 +55,8 @@ class BitChunk α where
 
     map     ∷ (Bool → Bool) → α → α
     reverse ∷ α → α
+
+    foldl ∷ (β → Bool → β) → β → α → β
 
 (∅) ∷ Chunk d
 (∅) = Chunk 0 0
@@ -85,26 +90,35 @@ instance BitChunk (Chunk Left) where
         = Chunk (n-1) (b `shiftR` 1)
 
     map _ (Chunk 0 _) = Chunk 0 0
-    map f (Chunk n b) = Chunk n (map' b (n-1))
+    map f (Chunk n b) = Chunk n (map_ b (n-1))
         where
-          map' ∷ Word8 → Int → Word8
-          map' b' 0
+          map_ ∷ Word8 → Int → Word8
+          map_ b' 0
               | f (testBit b' 0) = setBit   b' 0
               | otherwise        = clearBit b' 0
-          map' b' n'
-              | f (testBit b' n') = map' (setBit   b' n') (n'-1)
-              | otherwise         = map' (clearBit b' n') (n'-1)
+          map_ b' n'
+              | f (testBit b' n') = map_ (setBit   b' n') (n'-1)
+              | otherwise         = map_ (clearBit b' n') (n'-1)
 
     reverse (Chunk 0 _) = Chunk 0 0
-    reverse (Chunk n b) = Chunk n (rev' b (n-1))
+    reverse (Chunk n b) = Chunk n (rev_ b (n-1))
         where
-          rev' ∷ Word8 → Int → Word8
-          rev' b' 0
+          rev_ ∷ Word8 → Int → Word8
+          rev_ b' 0
               | testBit b 0 = setBit   b' (n-1)
               | otherwise   = clearBit b' (n-1)
-          rev' b' n'
-              | testBit b n' = rev' (setBit   b' (n-1-n')) (n'-1)
-              | otherwise    = rev' (clearBit b' (n-1-n')) (n'-1)
+          rev_ b' n'
+              | testBit b n' = rev_ (setBit   b' (n-1-n')) (n'-1)
+              | otherwise    = rev_ (clearBit b' (n-1-n')) (n'-1)
+
+    foldl _  i (Chunk 0 _) = i
+    foldl op i (Chunk n b) = foldl_ op i 0
+        where
+          foldl_ ∷ (β → Bool → β) → β → Int → β
+          foldl_ op' a n'
+              | n' ≡ n    = a
+              | otherwise = foldl_ op' (a `op'` testBit b n') (n'+1)
+          
 
 instance BitChunk (Chunk Right) where
     singleton True  = Chunk 1 0x80
@@ -149,6 +163,14 @@ instance BitChunk (Chunk Right) where
           rev' b' n'
               | testBit b n' = rev' (setBit   b' (n-1-n')) (n'+1)
               | otherwise    = rev' (clearBit b' (n-1-n')) (n'+1)
+
+    foldl _  i (Chunk 0 _) = i
+    foldl op i (Chunk n b) = foldl_ op i 7
+        where
+          foldl_ ∷ (β → Bool → β) → β → Int → β
+          foldl_ op' a n'
+              | n' ≡ (8-n) = a
+              | otherwise  = foldl_ op' (a `op'` testBit b n') (n'-1)
 
 unsafeInit ∷ Chunk d → Chunk d
 unsafeInit (Chunk n b) = Chunk (n-1) b
