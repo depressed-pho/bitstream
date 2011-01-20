cf860920a24d8ee82dbd7cd759cef0b86a3c42b7
PHO <pho@cielonegro.org>
2011-01-20 17:49:51 +0900

    Bitstream: stream / unstream

--- Data/Bitstream.hs
+++ Data/Bitstream.hs
@@ -24,6 +24,7 @@ import qualified Data.StorableVector as SV
 import qualified Data.StorableVector.Base as SV
 import qualified Data.Stream as S
 import Foreign.Storable
+import Prelude.Unicode
 
 newtype Bitstream d
     = Bitstream (SV.Vector (Packet d))
@@ -49,17 +50,41 @@ unstreamSV (S.Stream f s0) = SV.unfoldr consume s0
                     S.Skip    s' → consume s'
                     S.Done       → Nothing
 
-{-# INLINE unstreamSV' #-}
-unstreamSV' ∷ ∀α. Storable α ⇒ Int → S.Stream α → SV.Vector α
-unstreamSV' n (S.Stream f s0) = fst $ SV.unfoldrN n consume s0
+{-
+{-# INLINE unsafeUnstreamSV #-}
+unsafeUnstreamSV ∷ ∀α. Storable α ⇒ Int → S.Stream α → SV.Vector α
+unsafeUnstreamSV n (S.Stream f s0) = fst $! SV.unfoldrN n consume s0
     where
       {-# INLINE consume #-}
       consume s = case f s of
                     S.Yield α s' → Just (α, s')
                     S.Skip    s' → consume s'
                     S.Done       → Nothing
+-}
+
+{-# INLINE packStream #-}
+packStream ∷ ∀d. G.Bitstream (Packet d) ⇒ S.Stream Bool → S.Stream (Packet d)
+packStream (S.Stream f s0) = S.unfoldr pack8 (Just s0)
+    where
+      {-# INLINE pack8 #-}
+      pack8 Nothing  = Nothing
+      pack8 (Just s) = case G.unfoldrN (8 ∷ Int) consume s of
+                         (p, s')
+                             | G.null p  → Nothing
+                             | otherwise → Just (p, s')
+      {-# INLINE consume #-}
+      consume s = case f s of
+                    S.Yield b s' → Just (b, s')
+                    S.Skip    s' → consume s'
+                    S.Done       → Nothing
 
 instance G.Bitstream (Packet d) ⇒ G.Bitstream (Bitstream d) where
     {-# INLINE [0] stream #-}
     stream (Bitstream v)
-        = S.concatMap G.stream (streamSV v)
+        = {-# CORE "strict bitstream 'stream'" #-}
+          S.concatMap G.stream (streamSV v)
+
+    {-# INLINE [0] unstream #-}
+    unstream
+        = {-# CORE "strict bitstream 'unstream'" #-}
+          Bitstream ∘ unstreamSV ∘ packStream
